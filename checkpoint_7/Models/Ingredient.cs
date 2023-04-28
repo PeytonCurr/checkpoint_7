4596589d51be207b569c2ca9ca64691b70a157da
peyton <peyton.curr@gmail.com>
2023-04-28 12:22:35 -0600

    Added the table and all the files for Ingredients

---
+++ checkpoint_7/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+namespace checkpoint_7.Models;
+
+public class Ingredient
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string Quantity { get; set; }
+  public int RecipeId { get; set; }
+}
+
---
+++ checkpoint_7/Services/IngredientsService.cs
@@ -0,0 +1,11 @@
+namespace checkpoint_7.Services;
+
+public class IngredientsService
+{
+  private readonly IngredientsRepository _repo;
+
+  public IngredientsService(IngredientsRepository repo)
+  {
+    _repo = repo;
+  }
+}
