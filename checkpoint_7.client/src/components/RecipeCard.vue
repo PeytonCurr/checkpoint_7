4ddbb5b616e2e4dddf5d799497139eaba223363e
peyton <peyton.curr@gmail.com>
2023-05-02 16:56:36 -0600

    Have The Recipe Card Displaying to Page, But still need to style the incomming Recipes

---
+++ checkpoint_7.client/src/components/RecipeCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="row">
+    <div class="col-6">
+      <span class="bg-grey rounded p-1">{{ rec.category }}</span>
+    </div>
+    <div class="col-6"></div>
+    <div class="col-12"></div>
+  </section>
+</template>
+
+
+<script lang="ts">
+import { Recipe } from '../models/Recipe';
+
+export default {
+
+  props: {
+    rec: { type: Recipe, required: true }
+  },
+
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
